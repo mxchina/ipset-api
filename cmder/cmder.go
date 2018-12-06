0cc9a1748574b288ae927a5f5c7de6986a9ef881
mx <274819450@qq.com>
2018-12-06 21:17:17 +0800

    cmder add stderr

--- cmder/cmder.go
+++ cmder/cmder.go
@@ -6,27 +6,32 @@ import (
 )
 
 func Exec_shell(s string) (string, error) {
-	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	//该函数执行一个shell命令，如果成功返回成功信息，err为nil
+	//如果执行命令失败，返回shell的stderr提示，且err不为nil（类似exit code 127）
 	cmd := exec.Command("/bin/bash", "-c", s)
-
-	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
-	var out bytes.Buffer
+	var (
+		out    bytes.Buffer
+		errOut bytes.Buffer
+	)
+	// stdout获取标准输出，stderr获取错误输出
 	cmd.Stdout = &out
+	cmd.Stderr = &errOut
 
-	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
-	err := cmd.Run()
-	return out.String(), err
+	if err := cmd.Run(); err != nil {
+		return errOut.String(), err
+	}
+	return out.String(), nil
 }
 
 //func main() {
 //	if len(os.Args)!=0{
-//		fmt.Println(os.Args[0])// args 第一个片 是文件路径
+//		fmt.Printf("文件路径为：%s\n",os.Args[0])// args 第一个片 是文件路径
 //	}
 //	s := os.Args[1]
 //	res, err := Exec_shell(s)
 //	if err != nil {
-//		fmt.Println(err)
+//		fmt.Printf("命令执行错误，错误信息为：%s\n", res)
+//	}else {
+//		fmt.Printf("命令执行成功，返回信息为：%s\n", res)
 //	}
-//	fmt.Println("结果：")
-//	fmt.Printf("%q",res)
 //}
